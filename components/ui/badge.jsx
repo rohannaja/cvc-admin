2c9d78aa4e757720d8356a4551401bf8f6f6689e
Rohanna Apil <118111243+rohannaja@users.noreply.github.com>
2025-02-24 10:36:32 +0800

    initial commit

---
+++ components/ui/badge.jsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+import { cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils"
+
+const badgeVariants = cva(
+  " inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+        secondary:
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          pending:
+          "border-transparent bg-yellow-500 text-secondary-foreground hover:bg-yellow-600",
+          completed:
+          "border-transparent bg-green-500 text-secondary-foreground hover:bg-green-600",
+          rejected:
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        destructive:
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        outline: "text-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+function Badge({
+  className,
+  variant,
+  ...props
+}) {
+  return (<div className={cn(badgeVariants({ variant }), className)} {...props} />);
+}
+
+export { Badge, badgeVariants }
